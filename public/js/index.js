e89ba2f71a0951b3f77357b04aa08a3600a556f3
wajdomahdi <46847635+wajdomahdi@users.noreply.github.com>
2019-07-18 00:23:27 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,90 +1,40 @@
 
-const fs = require("fs");
-const path = require("path");
 
-const homeHandler = (request, response) => {
-  const filePath = path.join(__dirname, "..", "public", "index.html");
-  console.log("filepath", filePath);
-
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log("notdone");
-      response.writeHead(500, {
-        "Content-Type": "text/html"
-      });
-      response.end("<h1>server error</h1>");
-    } else {
-      console.log("done");
-      response.writeHead(200, {
-        "Content-Type": "text/html"
-      });
-      response.end(file);
-    }
-  });
-};
-
-const publicHandler = (request, response, url) => {
-  const extension = url.split(".")[1];
-  const extensionType = {
-    html: "text/html",
-    css: "text/css",
-    js: "application/javascript",
-    jpg: "img/jpg",
-    txt: "text/plain",
-    ico: "image/x-icon"
-  };
-
-  const filePath = path.join(__dirname, "..", "public", url);
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      response.writeHead(500, {
-        "Content-Type": "text/html"
-      });
-      response.end("<h1>server error</h1>");
-    } else {
-      response.writeHead(200, {
-        "Content-Type": extensionType[extension]
-      });
-      response.end(file);
-    }
-  });
-};
-
-const searchHandler = (request, response) => {
-  let inputValue = request.url.split("/")[2];
-  console.log("input text", inputValue);
-  const filePath = path.join(__dirname, "country.json");
-  console.log("search file path", filePath);
-  fs.readFile(filePath, "utf8", (error, file) => {
-    if (error) {
-      response.writeHead(500, {
-        "Content-Type": "text/html"
-      });
+function request(url, cb) {
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return cb(data);
+    })
+    .catch(error => {
       console.log(error);
-      response.end("<h1>server error</h1>");
-    } else if (inputValue.length > 0) {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      //const text = file.split('\n').map((x) => x);
-      const allList = JSON.parse(file);
-      const capitalizedFirstLetter = inputValue[0].toUpperCase();
-      // console.log("capital", capitalizedFirstLetter);
-      inputValue = capitalizedFirstLetter + inputValue.slice(1);
-      // console.log("search", inputValue);
-      //const filteredData = matchedResults(text, inputValue);
-      const filteredData = allList.filter(element => {
-        return element.name.indexOf(inputValue) === 0;
-      });
+    });
+}
+const countryInput = document.getElementById("country");
 
-      const country = JSON.stringify(filteredData);
-      //  console.log("filtered data search", filteredData);
-      console.log("file text", JSON.stringify(country));
-      response.end(country);
-    } else {
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(country));
+//const countryList = document.getElementById("section-res");
+
+const buttonsearch = document.getElementById("buttonsearch");
+country.addEventListener("input", function() {
+  //  console.log("hellllo");
+  const input = countryInput.value;
+  console.log("hellllo", input);
+  request(`/search/${input}`, data => {
+    console.log("dataa", data);
+
+    var ul = document.createElement("ul");
+    ul.setAttribute("id", "countryName");
+    for (var i = 0; i < data.length; i++) {
+      var li = document.createElement("li");
+      li.setAttribute("class", "item");
+      var valueofarray = document.createTextNode(data[i].name);
+      li.appendChild(valueofarray);
+      ul.appendChild(li);
     }
+    var sec = document.getElementById("section-res");
+    sec.innerHTML = "";
+    sec.appendChild(ul);
   });
-};
-
-module.exports = { homeHandler, publicHandler, searchHandler };
-_
+});
